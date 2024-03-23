8b54e4a0066b15b495962d222cc504b0d40501e6
Parthib <parthibbanerjee01@gmail.com>
2024-03-23 18:28:50 +0530

    Made the website responsive, added themes and dark mode

---
+++ script.js
@@ -0,0 +1,7 @@
+/*=========== Typing Animation ==========*/
+var typed = new Typed(".typing",{
+    strings:["","Web Developer", "AWS Solutions Architect Associate"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
---
+++ style-switcher.js
@@ -0,0 +1,43 @@
+/* ================= toggle style switcher ==============*/
+const styleSwitcherToggler = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggler.addEventListener("click", ()=>{
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+// hide style - switcher on scroll
+window.addEventListener("scroll", ()=>{
+    if(document.querySelector(".style-switcher").classList.contains("open"))
+    {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+})
+/* ================= Theme Color ==============*/
+const alternateStyles = document.querySelectorAll(".alternate-style");
+console.log(typeof alternateStyles)
+
+function setActiveStyle(color) {
+    alternateStyles.forEach((style) => {
+        if (color === style.getAttribute("title")) {
+            style.removeAttribute("disabled");
+        } else {
+            style.setAttribute("disabled", "true");
+        }
+    });
+}
+/* ================= Theme Light and Dark Mode ==============*/
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", ()=>{
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+})
+window.addEventListener("load", ()=>{
+    if(document.body.classList.contains("dark"))
+    {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else
+    {
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+})
+
